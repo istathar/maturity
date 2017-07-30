de22d8782c3fc68cdbd60fa59afc9be77befd95d
Andrew Cowie <andrew@operationaldynamics.com>
2017-07-30 22:28:13 +1000

    Work in progress tweak to snippet output

--- tests/Snippet.hs
+++ tests/Snippet.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module Main where
 
@@ -8,19 +9,23 @@ import qualified Data.Text.IO as T
 
 import Maturity.Scale
 
-model :: Model
-model = Model
-    (Technical CodeExists DesignChoices)
-    (Operational Discussion Insecure Undefined)
 
 vault = Model
     (Technical OthersKnow Deployable)
     (Operational Demo Enterprise Undefined)
 
+myself = Model
+    (Technical CodeExists DesignChoices)
+    (Operational Discussion Insecure Undefined)
+
+
 main :: IO ()
 main = do
-    putStrLn (show model)
-    T.putStrLn (render model)
-    putStrLn ""
-    putStrLn (show vault)
-    T.putStrLn (render vault)
+    T.putStrLn (describeModel vault)
+
+    T.putStrLn "vault"
+    T.putStrLn (outputScores vault)
+    T.putStrLn ""
+    T.putStrLn "myself"
+    T.putStrLn (outputScores myself)
+
