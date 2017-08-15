766955981cd03a05a20557eaa0bdfc1095fb2431
Andrew Cowie <andrew@operationaldynamics.com>
2017-08-15 22:11:06 +1000

    Experiments with Diagrams Cairo back end
    Successfully generate SVG and, more to the point, PDF image fragments.
    Nice! Experiment with relationships between font size, font face
    selection, newlines, and bounding box size.

--- tests/Example.hs
+++ tests/Example.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module Example (example) where
 
+import Data.String.Here
+import qualified Data.Text as T
 import Data.Typeable
 import Diagrams.Prelude
 import Diagrams.TwoD.Text
+
+import Text.Render (wrap)
 
 example
   :: (Typeable n, RealFloat n,
@@ -17,9 +22,43 @@ example
   => QDiagram b V2 n Any
 
 
+--str = "Hello for Apéx GO01"
+--This is a test of the Emergency Broadcast System.
+--MMMM MM M MMMM MM MMM MMMMMMM MMMMMMMMM 123456789 MMMMMMM
+str = [here|
+We've surveyed the available options, a technology
+choice has been made, and preliminary engineering
+design has been completed.
+|]
+
+enbox :: String -> String
+enbox = T.unpack . wrap 30 . T.pack
+
+
 --
 -- Has to be in a `rect` (or a something, anything) because Text, by itself,
 -- has no size, apparently
 --
-example = baselineText "Hello World" <> rect 8 1 # fc lightgreen # showOrigin
+example = text (enbox str) # fc blue 
+    # font "Linux Libertine O"
+    # fontSize (local 1)
+    <> rect 20 15 # fc lightgreen # showOrigin
 
+{-
+
+    # font "Linux Libertine O"
+    # font "DejaVu Serif"
+    # font "DejaVu Sans"
+    # font "Liberation Serif"
+    # font "Linux Biolinum O"
+    # font "Linux Libertine O"
+    # font "Linux Libertine Initials O"
+    # font "Linux Libertine Mono O"
+    # font "Inconsolata"
+    # font "Nimbus Sans L"
+    # font "URW Gothic L"
+
+    -- really?!?
+
+    # font "TeX Gyre Heros"
+-}
